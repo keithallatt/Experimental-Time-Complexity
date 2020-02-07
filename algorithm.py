6887225a89b0af4e8986badc536bf35b7d8ea42f
Keith Allatt <keith.allatt@gmail.com>
2020-02-06 19:35:06 -0500

    Added linear regression, prints approx power k, for matching O(n^k) algorithms

--- algorithm.py
+++ algorithm.py
@@ -5,6 +5,9 @@ import sys
 import subprocess
 import os
 
+import numpy as np
+#from sklearn.linear_model import LinearRegression
+from scipy import stats
 
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
 	"""
@@ -33,6 +36,17 @@ output_pipe = open(os.devnull, "w+")
 if len(sys.argv) > 1:
 	file_output = sys.argv[1]
 	output_pipe = open(file_output, "w+")
+
+   
+   
+def calc_lin_regress(n_value, t_value):
+	"""
+		Calculate the linear regression of 
+	"""
+	slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+	
+	return (slope, std_err)
+
 
 class AlgoTest:
 	def __init__(self, func, input_gen, sample_size, input_range):
@@ -75,19 +89,26 @@ def cubic_op(lst):
 		quadratic_op(lst)
 
 if __name__ == "__main__":
+	log_t = np.array([])
+	log_n = np.array([])
 	try:
 		input_range = [100, 100000]
 		sample_size = 500
 
-		test = AlgoTest(linear_op, gen_list, sample_size, input_range)
+		test = AlgoTest(quadratic_op, gen_list, sample_size, input_range)
 		if output_pipe is not None:
 			output_pipe.write("n,t,logn,logt\n")
 	
 		progress_bar_length = 50
 		printProgressBar(0, test.sample_size, prefix = 'Progress:', suffix = 'Complete', length = progress_bar_length)
 	
+		# store results
+
 		for line in test.test():
 			output_pipe.write(",".join([str(x) for x in [line[0],line[1], math.log(line[0]), math.log(line[1])]])+"\n")
+			
+			log_n = np.append(log_n, [[math.log(line[0])]])
+			log_t = np.append(log_t, [[math.log(line[1])]])
 
 			printProgressBar(test.current, test.sample_size, prefix = 'Progress:', suffix = 'Complete', length = progress_bar_length)
 	
@@ -97,4 +118,9 @@ if __name__ == "__main__":
 		
 		printProgressBar(test.current, test.sample_size, prefix = 'Progress:', suffix = 'Complete', length = progress_bar_length)
 		print(f"\nProcessed input saved to {output_pipe.name}\nProcess terminated via Keyboard Interrupt.")
+	finally:	
+		slope, intercept, r_value, p_value, std_err = stats.linregress(log_n,log_t)
+		confidence = 1-std_err
+		print(slope, confidence)
+
 
